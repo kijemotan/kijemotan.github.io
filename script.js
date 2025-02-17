f955401aacdca966a9394716aab3920900e394ac
the motan <kijemotan@gmail.com>
2025-02-17 04:16:55 +0100

    removed outdated random message

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const marqueeMsgs =
   "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
   "site budget does not exceed 12",
   "muuuuu",
-  "raccoon sound",
   "fun fact! this marquee is actually a transcription of what i'm saying irl right now. send me hate mail to test it :3c",
   "credit to tabby for the random message script",
   "oh crap",
